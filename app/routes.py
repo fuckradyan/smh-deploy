c886615acdabc081081df4f5f419d1e586201067
fuckradyan <artem.fagradyan@mail.ru>
2021-12-05 01:30:31 +0300

    add links section

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import send_from_directory, render_template, Flask, request
 app = Flask(__name__)
+
 @app.route('/')
 @app.route('/index')
 def index():
